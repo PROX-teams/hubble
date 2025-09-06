912c9bc692e4eb9a73f41cab76c1ed31f89f48c7
JJJ-un <jhjk7272@naver.com>
2025-09-06 22:47:49 +0900

    docs: input 컴포넌트 파라미터 주석 추가[BETA-95]

--- src/shared/ui/input/input/Input.tsx
+++ src/shared/ui/input/input/Input.tsx
@@ -5,6 +5,24 @@ import { RecipeVariants } from "@vanilla-extract/recipes";
 import clsx from "clsx";
 import * as S from "./Input.css";
 
+/**
+ * 
+ * @param {"solid"} [variant] - input의 스타일 변형. 
+ *   - "solid": 배경색 `vars.color.gray_100`과 보더 `0.063rem solid ${vars.color.stroke_200}`를 적용.
+ *   - 생략 시 base 스타일이 적용.
+ * 
+ * @param label - input 필드에 대한 설명 레이블을 지정.
+ * 
+ * @param {"sm" | "md" | "lg"} [size="md"] - 인풋 박스의 크기(높이).
+ *   - sm: 높이 2.13rem
+ *   - md: 높이 2.25rem
+ *   - lg: 높이 2.75rem
+ * 
+ * @param isError - 에러 상태를 나타내는 불리언 값.
+ * 
+ * @param errorMessage - 에러 상태일 때 표시할 에러 메시지를 지정.
+ */
+
 type InputVariants = NonNullable<RecipeVariants<typeof S.inputRecipe>>;
 type Variant = NonNullable<InputVariants["variant"]>;
 
