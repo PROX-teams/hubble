66d5639c8f969646b82295aff2c3d28f58e61e8e
김예안 <kjan0929@naver.com>
2025-09-14 15:01:58 +0900

    refactor: media 반응형 유틸 함수 min, max 타입 및 주석 추가 [BETA-112]

--- src/shared/styles/responsive.css.ts
+++ src/shared/styles/responsive.css.ts
@@ -9,11 +9,23 @@ export const breakpoints = {
 
 type Breakpoints = keyof typeof breakpoints;
 
+/**
+ *
+ * @param breakpoint - 브레이크포인트 (1280, 1440, 1728, 1920)
+ * @param styles - 적용할 스타일 객체
+ * @param type - 미디어 쿼리 타입 ("min" 또는 "max"), 기본값은 "min"
+ * @returns - 미디어 쿼리를 포함한 스타일 객체
+ * @example
+ * media(1280, { color: 'red' }) // min-width: 1280px
+ * media(1440, { color: 'blue' }, 'max') // max-width: 1440px
+ */
+
 export const media = (
   breakpoint: Breakpoints,
-  styles: StyleRule
+  styles: StyleRule,
+  type: "min" | "max" = "min"
 ): StyleRule => ({
   "@media": {
-    [`screen and (min-width: ${breakpoints[breakpoint]})`]: styles,
+    [`screen and (${type}-width: ${breakpoints[breakpoint]})`]: styles,
   },
 });
