2f53b464c4eeee47c3060c0399ce996b1730c453
김예안 <kjan0929@naver.com>
2025-09-16 23:41:26 +0900

    feat: 알림 드롭다운 ui 구현 및 스토리 작성 [BETA-113]

---
+++ src/widgets/alarm-dropdown/AlarmDropdown.css.ts
@@ -0,0 +1,61 @@
+import { tx } from "@/shared/styles/textStyle.css";
+import { darkTheme, lightTheme, vars } from "@/shared/styles/theme.css";
+import { style } from "@vanilla-extract/css";
+
+export const dropdownMenu = style({
+  display: "flex",
+  flexDirection: "column",
+  height: "27.0625rem",
+  backgroundColor: vars.color.gray_100,
+});
+
+export const container = style({
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+  overflowY: "auto",
+});
+
+export const header = style({
+  position: "sticky",
+  display: "flex",
+  alignItems: "center",
+  gap: "0.25rem",
+  width: "100%",
+  minHeight: "2.875rem",
+  paddingLeft: "0.625rem",
+  backgroundColor: vars.color.gray_200,
+  borderBottom: `1px solid ${vars.color.stroke_200}`,
+});
+
+export const tabMenu = style([
+  tx.cap1_md,
+  {
+    width: "3.0625rem",
+    height: "1.625rem",
+    borderRadius: "0.375rem",
+    transition: "all 0.4s ease",
+    color: vars.color.gray_400,
+  },
+]);
+
+export const activeTabMenu = style([
+  tx.cap1_sb,
+  {
+    backgroundColor: vars.color.stroke_200,
+
+    selectors: {
+      [`${darkTheme} &`]: {
+        color: vars.color.white,
+      },
+      [`${lightTheme} &`]: {
+        color: vars.color.black,
+      },
+    },
+  },
+]);
+
+export const content = style({
+  overflowY: "auto",
+  padding: "0.5rem 0 1rem 0",
+});
---
+++ src/widgets/alarm-dropdown/AlarmDropdown.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import clsx from "clsx";
+import { Dropdown } from "@/shared/ui/dropdown/Dropdown";
+import AlarmDropdownTrigger from "@/features/alarm/toggle-dropdown/ui/AlarmDropdownTrigger";
+import * as S from "./AlarmDropdown.css";
+import { Tab } from "@/shared/ui/tab-menu/TabMenuRoot";
+import { ALARM_TABS } from "@/entities/alarm/alarm.constants";
+import { Alarm } from "@/entities/alarm/alarm.types";
+import AlarmCard from "@/entities/alarm/ui/AlarmCard";
+import MarkAsDoneButton from "@/features/alarm/mark-as-done/ui/MarkAsDoneButton";
+
+interface AlarmDropdownProps {
+  data: Alarm[]; // 알림 데이터를 배열로 받습니다.
+  independent?: boolean | null; // 메뉴를 Context와 무관하게 독립적으로 열지 여부를 설정합니다.
+}
+
+export default function AlarmDropdown({
+  data,
+  independent,
+}: AlarmDropdownProps) {
+  return (
+    <Dropdown>
+      {/* 알림 드롭다운 트리거 버튼 */}
+      <AlarmDropdownTrigger />
+
+      <Dropdown.Menu
+        placement="right"
+        size="3xl"
+        className={S.dropdownMenu}
+        independent={independent ?? undefined}
+      >
+        <Tab className={S.container}>
+          <Tab.Menus className={S.header}>
+            <Tab.Menu
+              tabIndex={ALARM_TABS.INBOX}
+              className={clsx(S.tabMenu)}
+              activeStyle={S.activeTabMenu}
+            >
+              Inbox
+            </Tab.Menu>
+            <Tab.Menu
+              tabIndex={ALARM_TABS.DONE}
+              className={clsx(S.tabMenu)}
+              activeStyle={S.activeTabMenu}
+            >
+              Done
+            </Tab.Menu>
+          </Tab.Menus>
+
+          <Tab.Contents className={S.content}>
+            {data.map((alarm, idx) => (
+              <Tab.Content
+                key={idx} // 실제 데이터 연동 시 고유 ID로 변경 필요
+                tabIndex={alarm.isDone ? ALARM_TABS.DONE : ALARM_TABS.INBOX}
+              >
+                <AlarmCard {...alarm} actionSlot={<MarkAsDoneButton />} />
+              </Tab.Content>
+            ))}
+          </Tab.Contents>
+        </Tab>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
