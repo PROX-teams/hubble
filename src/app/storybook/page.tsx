e504d1eb96f9b5322050e6688adbe5bfa9f623b0
JJJ-un <jhjk7272@naver.com>
2025-09-23 10:00:44 +0900

    chore: 폴더 위치 변경[BETA-100]

--- src/app/storybook/page.tsx
+++ src/app/storybook/page.tsx
@@ -1,8 +1,8 @@
-import StoryCardGrid from "@/features/storybook/ui/StoryCardContent";
+import StoryCardGrid from "@/entities/story/ui/story-card/StoryCardContent";
 import data from "@/shared/ui/storycard/mock.json";
 import type { StoryEntity } from "@/shared/types/story.types";
 
-const stories: StoryEntity[] = Array(0).fill(data).map((item, idx) => ({
+const stories: StoryEntity[] = Array(9).fill(data).map((item, idx) => ({
   ...item,
   id: idx + 1, 
 }));
--- src/entities/story/ui/story-card/StoryCardContent.css.ts
+++ src/entities/story/ui/story-card/StoryCardContent.css.ts
@@ -5,9 +5,7 @@ export const gridContainer = style([{
   display: "grid",
   gridTemplateColumns: "repeat(2, minmax(0, 1fr))", 
   width: "824px",
-  minHeight: "170px",
   gap: "20px", 
-  margin: "0 72px",
   },
   media(1280, {
       gridTemplateColumns: "repeat(2, minmax(0, 1fr))", 
@@ -15,15 +13,12 @@ export const gridContainer = style([{
   }),
     media(1440, {
       width: "984px",
-      margin: "0 calc(72px + (100vw - 1440px)/2)",
   }),
     media(1490, {
       gridTemplateColumns: "repeat(3, minmax(0, 1fr))", 
-      margin: "0 97px",
       width: "calc(984px + (100vw - 1490px))",
   }),
     media(1728, {
       width: "1222px",
-      margin: "0 calc(97px + (100vw - 1728px)/2)", 
   }),
 ]);
