51b553624c06b2c9f78f60eaba4649e2e3f85d89
JJJ-un <jhjk7272@naver.com>
2025-09-30 15:32:36 +0900

    chore: 스토리북 페이지 컴포넌트 복구[BETA-100]

--- src/app/storybook/page.tsx
+++ src/app/storybook/page.tsx
@@ -1,13 +1,3 @@
-import StoryCardGrid from "@/widgets/storycard-container/StoryCardContainer";
-import data from "@/shared/mock/story.json";
-import type { StoryEntity } from "@/entities/story/story.types";
-
-const stories: StoryEntity[] = Array(9).fill(data).map((item, idx) => ({
-  ...item,
-  id: idx + 1, 
-}));
-
 export default function StorybookPage() {
-  return <StoryCardGrid stories={stories} />;
+  return <div>StorybookPage</div>;
 }
-  
