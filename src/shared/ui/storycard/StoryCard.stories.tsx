cc3a0b265bdbc1bec949c38bdaf68f31c12d36a0
JJJ-un <jhjk7272@naver.com>
2025-09-30 14:30:21 +0900

    docs: size props에 따른 스토리북 추가작성[BETA-100]

--- src/shared/ui/storycard/StoryCard.stories.tsx
+++ src/shared/ui/storycard/StoryCard.stories.tsx
@@ -11,7 +11,7 @@ const meta: Meta<typeof StoryCard> = {
       description: {
         component:
           "스토리의 아이콘, 제목, 설명, 연결된 아티클 수 등 핵심 정보를 요약하여 보여주는 카드 컴포넌트입니다." +`\n`+
-          "props로 전달된 데이터(StoryEntity)를 기반으로 렌더링됩니다."
+          "props로 전달된 data(StoryEntity)를 기반으로 렌더링되며, size를 통해 카드의 크기를 'small' 또는 'large'로 조절할 수 있습니다.",
       },
     },
   },
@@ -25,6 +25,15 @@ const meta: Meta<typeof StoryCard> = {
         defaultValue: { summary: "{}" },
       },
     },
+    size: {
+      control: { type: "radio" },
+      options: ["small", "large"],
+      description: "카드 크기",
+      table: {
+        type: { summary: "'small' | 'large'" },
+        defaultValue: { summary: "'large'" },
+      },
+    },
     onClick: {  action: "clicked" },
   },
 } satisfies Meta<typeof StoryCard>;
@@ -33,7 +42,12 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {data},
-  render: (args) => <div style={{ width: "402px" }}><StoryCard {...args} /></div>,
+export const Large: Story = {
+  args: {data, size: "large"},
+  render: (args) => <div style={{ width: "394px" }}><StoryCard {...args} /></div>,
+};
+
+export const Small: Story = {
+  args: {data, size: "small"},
+  render: (args) => <div style={{ width: "414px" }}><StoryCard {...args} /></div>,
 };
