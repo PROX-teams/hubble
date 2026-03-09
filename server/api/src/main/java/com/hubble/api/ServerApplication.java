14ace41d32ef115e365bc3ae9f78bca4b8b75640
JJJ-un <jhjk7272@naver.com>
2026-03-10 01:29:58 +0900

    chore: api 및 common 멀티 모듈 설정 및 패키지 구조 재구성

---
+++ server/api/src/main/java/com/hubble/api/ServerApplication.java
@@ -0,0 +1,17 @@
+package com.hubble.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.hubble")
+@EntityScan(basePackages = "com.hubble.common.entity")
+@EnableJpaRepositories(basePackages = "com.hubble.common.repository")
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
