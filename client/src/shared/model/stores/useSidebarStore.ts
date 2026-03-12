691eb80c202784c8a70a6cab2a1404d0714d0248
JJJ-un <jhjk7272@naver.com>
2026-03-12 16:03:31 +0900

    chore: 삭제된 useSidebarStore.ts 복구

---
+++ client/src/shared/model/stores/useSidebarStore.ts
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+
+// 사이드바의 열림/닫힘 상태를 관리하는 전역 스토어입니다.
+type SidebarState = {
+  isSidebarOpen: boolean;
+  openSidebar: () => void;
+  closeSidebar: () => void;
+  toggleSidebar: () => void;
+};
+
+export const useSidebarStore = create<SidebarState>((set) => ({
+  isSidebarOpen: false,
+  openSidebar: () => set({ isSidebarOpen: true }),
+  closeSidebar: () => set({ isSidebarOpen: false }),
+  toggleSidebar: () => set((s) => ({ isSidebarOpen: !s.isSidebarOpen })),
+}));
