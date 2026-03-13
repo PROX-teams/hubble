2ad15514002763a64f8d0e9bc7e48a5a7f104dfc
JJJ-un <jhjk7272@naver.com>
2026-03-13 15:02:11 +0900

    fix: CORS 허용 목록에 서버 IP 추가 [RELEASE-56]

--- server/api/src/main/java/com/hubble/config/SecurityConfig.java
+++ server/api/src/main/java/com/hubble/config/SecurityConfig.java
@@ -79,7 +79,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:8080",
+                "http://3.36.64.194:8080"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
         configuration.setAllowCredentials(true);
